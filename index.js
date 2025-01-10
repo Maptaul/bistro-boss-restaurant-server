5b6a1d04e8bbd146b0b34cc035ccec231945c2ff
@maptaul <maptaulislam1@gmail.com>
2025-01-10 19:41:58 +0600

    add

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ async function run() {
     const verifyToken = (req, res, next) => {
       console.log("inside verify token", req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "forbidden access" });
+        return res.status(401).send({ message: "unauthorized access" });
       }
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "forbidden access" });
+          return res.status(401).send({ message: "unauthorized access" });
         }
         req.decoded = decoded;
         next();
@@ -67,7 +67,7 @@ async function run() {
     app.get("/admin/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       if(email !== req.decoded.email){
-        return res.status(403).send().send({ message: "unauthorized access" });
+        return res.status(403).send().send({ message: "forbidden access" });
       }
       const query = {email: email};
       const user = await userCollection.findOne(query);
