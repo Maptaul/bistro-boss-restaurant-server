1c380494fc0958d6306100da91460ecf28d79840
@maptaul <maptaulislam1@gmail.com>
2025-01-13 22:30:22 +0600

    add

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartsCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payments");
 
     //jwt related apis
     app.post("/jwt", async (req, res) => {
@@ -209,6 +210,24 @@ async function run() {
         clientSecret: paymentIntent.client_secret,
       });
     });
+
+    // payment related api
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      //  carefully delete each item from the cart
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
+
+      const deleteResult = await cartsCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
